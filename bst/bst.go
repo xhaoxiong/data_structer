b96039b392be0bb24662ad68d417a670ec2fe336
haoxiongxiao <269812428@qq.com>
2018-10-07 17:17:15 +0800

    先序遍历

--- bst/bst.go
+++ bst/bst.go
@@ -5,10 +5,16 @@
 */
 package bst
 
+import (
+	"fmt"
+	"github.com/spf13/cast"
+)
+
 type BST struct {
 	root *node
 	node
 	Size int
+	Str  string
 }
 
 type node struct {
@@ -27,16 +33,73 @@ func (this *BST) Add(elem int) {
 
 }
 
-func (this *BST) add(node *node, elem int) *node {
-	if node == nil {
+func (this *BST) add(Node *node, elem int) (n *node) {
+	if Node == nil {
 		this.Size++
 		return &node{elem: elem}
 	}
 
-	if elem < node.elem {
-		node.left = this.add(node.left, elem)
-	} else if elem > node.elem {
-		node.right = this.add(node.right, elem)
+	if elem < Node.elem {
+		Node.left = this.add(Node.left, elem)
+	} else if elem > Node.elem {
+		Node.right = this.add(Node.right, elem)
 	}
-	return node
+	return Node
+}
+
+func (this *BST) Contains(elem int) bool {
+	return this.contains(this.root, elem)
+}
+
+func (this *BST) contains(node *node, elem int) bool {
+	if node == nil {
+		return false
+	}
+
+	if node.elem == elem {
+		return true
+	} else if elem < node.elem {
+		return this.contains(node.left, elem)
+	} else {
+		return this.contains(node.right, elem)
+	}
+
+}
+
+func (this *BST) PreOrder() {
+
+}
+
+func (this *BST) preOrder(Node *node) {
+	if Node == nil {
+		return
+	}
+
+	fmt.Println(Node.elem)
+	this.preOrder(Node.left)
+	this.preOrder(Node.right)
+}
+
+func (this *BST) ToString() string {
+	return this.GenerateBSTString(this.root, 0)
+}
+
+func (this *BST) GenerateBSTString(Node *node, depth int) string {
+	if Node == nil {
+		this.Str += this.GenerateDepthString(depth) + "nil\n"
+		return this.Str
+	}
+
+	this.Str += this.GenerateDepthString(depth) + cast.ToString(Node.elem) + "\n"
+	this.Str += this.GenerateBSTString(Node.left, depth+1)
+	this.Str += this.GenerateBSTString(Node.right, depth+1)
+	return this.Str
+}
+
+func (this *BST) GenerateDepthString(depth int) string {
+	str := ""
+	for i := 0; i < depth; i++ {
+		str += "--"
+	}
+	return str
 }
--- bst/bst_test.go
+++ bst/bst_test.go
@@ -4,3 +4,36 @@
 *@Description: CREATE GO FILE bst
 */
 package bst
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestBST_Add(t *testing.T) {
+	bst := &BST{}
+	var nums = []int{5, 3, 6, 8, 4, 2}
+	for _, num := range nums {
+		bst.Add(num)
+	}
+
+	fmt.Print(bst.ToString())
+	fmt.Println(bst.Contains(10))
+	fmt.Println(bst.Contains(5))
+}
+
+/*
+5
+--3
+----2
+------nil
+------nil
+----4
+------nil
+------nil
+--6
+----nil
+----8
+------nil
+------nil
+*/
